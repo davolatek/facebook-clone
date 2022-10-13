9e407acabd53b7a432129630427aa45988f74f73
= <=dave.jodatek@gmail.com>
2022-10-13 01:40:01 +0100

    created springboot server

---
+++ server/src/test/java/com/knowledgetekhub/facebookclone/FacebookCloneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledgetekhub.facebookclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCloneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
